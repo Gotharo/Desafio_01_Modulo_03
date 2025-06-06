d67051f4158c4aa2bc8a9400dd872e6cab6fbfeb
Gonzalo Castro <gonzalo.castro@gotharo.cl>
2025-06-06 12:26:33 -0400

    feature: se realiza ejercicio de conteo

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,15 @@ precioSpan.innerHTML = precio
 
 let count = 0;
 
+document.querySelector("#btn-suma").addEventListener("click", () => {
+    count ++;
+    document.querySelector(".cantidad").innerHTML = count;
+    document.querySelector(".valor-total").innerHTML = count * precio;
+});
+document.querySelector("#btn-resta").addEventListener("click", () => {
+    if (count > 0) {
+        count --;
+        document.querySelector(".cantidad").innerHTML = count;
+        document.querySelector(".valor-total").innerHTML = count * precio;
+    }
+});
