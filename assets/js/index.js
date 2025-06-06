e65f213ed0f6280bb8209beaee6ea68bb2915d19
Gonzalo Castro <gonzalo.castro@gotharo.cl>
2025-06-06 12:03:28 -0400

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,16 +5,3 @@ precioSpan.innerHTML = precio
 
 let count = 0;
 
-document.querySelector("#btnPlus").addEventListener("click", () => {
-    count++;
-    document.querySelector(".cantidad").innerHTML = count;
-    document.querySelector(".valor-total").innerHTML = count * precio;
-});
-
-document.querySelector("#boton-restar").addEventListener("click", () => {
-    if (count > 0) {
-        count--;
-        document.querySelector(".cantidad").innerHTML = count;
-        document.querySelector(".valor-total").innerHTML = count * precio;
-    }
-});
