cc3a6f7c8248902dee5ffd4ced7838addc1398d7
Gonzalo Castro <gonzalo.castro@gotharo.cl>
2025-06-06 11:31:22 -0400

    feature: se agrega el contador requerido en el sdafio

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,19 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+let count = 0;
+
+document.querySelector("#btnPlus").addEventListener("click", () => {
+    count++;
+    document.querySelector(".cantidad").innerHTML = count;
+    document.querySelector(".valor-total").innerHTML = count * precio;
+});
+
+document.querySelector("#boton-restar").addEventListener("click", () => {
+    if (count > 0) {
+        count--;
+        document.querySelector(".cantidad").innerHTML = count;
+        document.querySelector(".valor-total").innerHTML = count * precio;
+    }
+});
